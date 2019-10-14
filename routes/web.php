8835409a85da2b37d5cd2fc1a79c8b4f05a48b71
Imran Hossain <imran.dev.bd@gmail.com>
2019-10-15 04:16:25 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('admin/home', 'HomeController@adminHome')->name('admin.home')->middleware('is_admin');
